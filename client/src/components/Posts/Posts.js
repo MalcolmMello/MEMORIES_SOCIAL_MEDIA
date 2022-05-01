db6abcf37480666d3758206ffa82f8008fa784d8
MalcolmMello <malcolmmellolima@gmail.com>
2022-05-01 14:37:28 -0300

    crud for posts completely done

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid, CircularProgress } from '@material-ui/core'
 import { useSelector } from "react-redux";
 import Post from "./Post/Post.js";
 import useStyles from './styles';
 
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
@@ -14,7 +14,7 @@ const Posts = () => {
             <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
                 { posts.map((post) => (
                     <Grid key={post._id} item xs={12} sm={6}>
-                        <Post post={post} />
+                        <Post post={post} setCurrentId={setCurrentId} />
                     </Grid>
                 )) }
             </Grid>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
@@ -22,4 +23,39 @@ export const createPost = async (req, res) => {
     } catch(error) {
         res.status(409).json({ message: error.message });
     };
+};
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    const { title, message, creator, selectedFile, tags } = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatePost = { title, message, creator, selectedFile, tags };
+
+    await PostMessage.findByIdAndUpdate(_id, updatePost, { new: true });
+    
+    res.json(updatePost);
+};
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted sucessfuly!' });
+};
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
 };
