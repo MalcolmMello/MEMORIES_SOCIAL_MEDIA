6bf9e3c8621696dacb60adf888fb5a08be958641
Malcolm Lima <malcolmmellolima@gmail.com>
2022-06-02 21:02:25 -0300

    pagination

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Pagination from '../Pagination';
 
 import useStyles from './styles';
 
@@ -27,6 +28,9 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper elevation={6}>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,10 @@
+const PostDetails = () => {
+    console.log('postdetails')
+    return (
+        <div>
+            post details
+        </div>
+    );
+}
+
+export default PostDetails
